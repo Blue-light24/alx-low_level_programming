f6a3c93f507960fa0a4b92819fe28e8b41c24075
mohcine <oussaidmohcine@gmail.com>
2023-06-17 02:50:10 +0100

    no time that is why i copy but i will study soon

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase followed by uppercase.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c = 'a';
+	char b = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0 to 9.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
